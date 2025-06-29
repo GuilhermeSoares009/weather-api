78e7823024c723077def852f7fcc1b3436e7b845
Guilherme Soares <guilherme.sousa009@hotmail.com>
2025-06-29 03:21:00 -0300

    feat: adicionado as rotas + testando a chamada de rotas com um exemplo simples + gitignore

--- public/index.php
+++ public/index.php
@@ -1,4 +1,17 @@
 <?php
-echo "Weather API funcionando!";
-phpinfo();
-?>
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use WheatherApi\Router;
+use WheatherApi\Controllers\WeatherController;
+
+$router = new Router();
+$weatherController = new WeatherController();
+
+$router->addRoute('GET', '/health', [$weatherController, 'health']);
+
+$method = $_SERVER['REQUEST_METHOD'];
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$router->dispatch($method, $path);
+
